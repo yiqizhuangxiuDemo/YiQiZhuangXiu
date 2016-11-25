a8410bfd3c8e2f04001ee18be0100983c3166471
zreo-seven <960971564@qq.com>
2016-11-25 20:00:11 +0800

    yongfei aa

--- app/src/main/java/com/bwf/yiqizhuangxiu/gui/activity/SearchActivity.java
+++ app/src/main/java/com/bwf/yiqizhuangxiu/gui/activity/SearchActivity.java
@@ -1,12 +1,29 @@
 package com.bwf.yiqizhuangxiu.gui.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.bwf.yiqizhuangxiu.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/11/23.
  */
 
-public class SearchActivity extends BaseActivity{
+public class SearchActivity extends BaseActivity {
+    @Bind(R.id.inner_search_eduittext)
+    EditText innerSearchEduittext;
+    @Bind(R.id.cancel_action)
+    TextView cancelAction;
+    @Bind(R.id.inner_search_delete)
+    ImageView innerSearchDelete;
+
     @Override
     protected int getContentViewResId() {
         return R.layout.activity_search;
@@ -20,5 +37,30 @@ public class SearchActivity extends BaseActivity{
     @Override
     protected void initDatas() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+
+
+    @OnClick({R.id.inner_search_eduittext, R.id.inner_search_delete, R.id.cancel_action})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.inner_search_eduittext:
+                innerSearchDelete.setVisibility(View.VISIBLE);
+                break;
+            case R.id.inner_search_delete:
+                innerSearchEduittext.setText("");
+                innerSearchDelete.setVisibility(View.GONE);
+                break;
+            case R.id.cancel_action:
+                finish();
+                break;
+        }
     }
 }
